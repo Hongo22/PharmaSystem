e1dac1bd9bda244b00f1550c41926fcbbaae2194
Hongo22 <cristianc.mirasierra@gmail.com>
2024-05-19 18:39:23 -0600

    Add files via upload

---
+++ conn_doc.php
@@ -0,0 +1,21 @@
+<?php
+
+	// Varaibles de conexión
+	$host = "localhost";
+	$usuario = "root";
+	$contraseña = "";
+	$base_datos = "pharmasystem";
+
+	// Conexión
+	$mysqli = new mysqli($host, $usuario, $contraseña, $base_datos);
+
+	// Verificar conexión
+	if ($mysqli->connect_error){
+		die("Error de conexión: " . $mysqli->connect_error);
+		exit();
+	}
+
+	//Ejecutar Consulta
+	$sql = "SELECT * FROM doctor WHERE eliminado = 0";
+	$resultado = $mysqli->query($sql);
+?>
